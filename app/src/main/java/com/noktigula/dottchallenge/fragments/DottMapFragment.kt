30ed023119cdadcbf37373200b8c7121e49965af
Artem <noktigula@gmail.com>
2020-01-19 22:24:26 +0100

    Limit min zoom preference

--- app/src/main/java/com/noktigula/dottchallenge/fragments/DottMapFragment.kt
+++ app/src/main/java/com/noktigula/dottchallenge/fragments/DottMapFragment.kt
@@ -5,30 +5,19 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.Observer
-import androidx.lifecycle.ViewModelProviders
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.MapView
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.model.LatLng
-import com.noktigula.dottchallenge.network.FoursquareApi
-import com.noktigula.dottchallenge.viewmodels.MapViewModel
-import retrofit2.Retrofit
-import retrofit2.converter.gson.GsonConverterFactory
-import com.facebook.stetho.okhttp3.StethoInterceptor
-import com.google.android.gms.maps.model.LatLngBounds
 import com.google.android.gms.maps.model.Marker
-import com.google.android.gms.maps.model.MarkerOptions
 import com.noktigula.dottchallenge.*
-import com.noktigula.dottchallenge.data.Repository
 import com.noktigula.dottchallenge.model.MapMarker
-import com.noktigula.dottchallenge.network.RetrofitInstance
-import com.noktigula.dottchallenge.viewmodels.MapViewModelFactory
-import okhttp3.OkHttpClient
 
-private const val DEFAULT_ZOOM = 15f
+private const val STREET = 15f
+private const val CITY = 10f
+private const val DEFAULT_ZOOM = STREET
 
 class DottMapFragment : Fragment(), OnMapReadyCallback {
     private val mapView: MapView by lazy { view!!.findViewById<MapView>(R.id.map_view) }
@@ -76,6 +65,8 @@ class DottMapFragment : Fragment(), OnMapReadyCallback {
         map.setOnMapClickListener {
             mapsActivity.selectedViewModelFactory.selectedVenue.value = null
         }
+
+        map.setMinZoomPreference(CITY)
     }
 
 

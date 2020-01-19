abaacf6155ba36dd4a96f66c95fc6cc311c85347
Artem <noktigula@gmail.com>
2020-01-19 13:16:38 +0100

    Map migrated into a custom fragment

---
+++ app/src/main/java/com/noktigula/dottchallenge/fragments/DottMapFragment.kt
@@ -0,0 +1,45 @@
+package com.noktigula.dottchallenge.fragments
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.MapView
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
+import com.noktigula.dottchallenge.R
+
+class DottMapFragment : Fragment(), OnMapReadyCallback {
+    private val mapView: MapView by lazy { view!!.findViewById<MapView>(R.id.map_view) }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_dott_map, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        mapView.onCreate(savedInstanceState)
+        mapView.getMapAsync(this)
+    }
+
+    override fun onResume() {
+        super.onResume()
+        mapView.onResume()
+    }
+
+    override fun onMapReady(inMap: GoogleMap?) {
+        val map = inMap ?: return
+
+        // Add a marker in Sydney and move the camera
+        val sydney = LatLng(-34.0, 151.0)
+        map.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
+        map.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+    }
+}
